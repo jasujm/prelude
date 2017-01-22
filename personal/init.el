be1fd1c5a3034adcf00a437bb0073309c910e2c0
Jaakko Moisio <jaakko@moisio.fi>
2017-01-22 15:35:34 +0200

    Remove personal yasnippet

--- personal/init.el
+++ personal/init.el
@@ -8,7 +8,5 @@
 (setq git-commit-fill-column 80)
 
 ;; Yasnippet
-; I have some problems installing yasnippet from MELPA. Working around...
-(add-to-list 'load-path "~/.emacs.d/personal/yasnippet")
 (require 'yasnippet)
 (yas-global-mode 1)
