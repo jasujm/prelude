f22342ea9c1755e49573658519d063dbe0dadcdc
Jaakko Moisio <jaakko@moisio.fi>
2020-10-25 07:24:44 +0100

    Add typescript and scss

--- personal/prelude-modules.el
+++ personal/prelude-modules.el
@@ -33,7 +33,8 @@
 ;; (require 'prelude-scala)
 ;; (require 'prelude-scheme)
 ;; (require 'prelude-shell)
-;; (require 'prelude-scss)
+(require 'prelude-scss)
+(require 'prelude-ts)
 ;; (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
 ;; (require 'prelude-yaml)
