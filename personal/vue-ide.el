dbbebca12ed9ab9d9396d927d77bb4191bbfb4e2
Jaakko Moisio <jaakko@moisio.fi>
2020-10-25 07:17:27 +0100

    wip

---
+++ personal/vue-ide.el
@@ -0,0 +1,16 @@
+(prelude-require-packages '(vue-mode lsp-mode))
+
+(require 'lsp-mode)
+
+(use-package lsp-mode
+             :commands lsp)
+
+;; for completions
+(use-package company-lsp
+             :after lsp-mode
+             :config (push 'company-lsp company-backends))
+
+(use-package vue-mode
+             :mode "\\.vue\\'"
+             :config
+             (add-hook 'vue-mode-hook #'lsp))
